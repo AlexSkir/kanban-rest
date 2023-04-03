4dbb3d857d57b1a5de426ca8420f4201881bd9dc
AlexSkir <a.skirnevskaia@gmail.com>
2023-04-03 10:08:16 +0300

    env update

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -6,8 +6,8 @@ dotenv.config({
   path: path.join(__dirname, '.env'),
 });
 
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT, POSTGRES_HOST } = process.env;
-const LOCAL_URL = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`;
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT, POSTGRES_HOST, POSTGRES_APPLICATION } = process.env;
+const LOCAL_URL = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}?application_name=${POSTGRES_APPLICATION}`;
 
 export default {
   type: 'postgres',
